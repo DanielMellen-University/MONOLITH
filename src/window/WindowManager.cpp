64abbc550fcdc764db59346a8e53da0984946959
DanielMellen-University <113362200+DanielMellen-University@users.noreply.github.com>
2026-09-05 22:06:48 -0600

    feat(shell): WindowManager.cpp includes restored body chunks
    Replace PLACEHOLDER_CPP with thin TU that includes detail/*.inc.

--- src/window/WindowManager.cpp
+++ src/window/WindowManager.cpp
@@ -1,1 +1,29 @@
-PLACEHOLDER_CPP
+#include "WindowManager.hpp"
+#include "../app/App.hpp"
+#include "../app/TerminalApp.hpp"
+#include "../app/TextEditorApp.hpp"
+#include "../app/FilesystemApp.hpp"
+#include "../app/SettingsApp.hpp"
+#include "../app/DrawingApp.hpp"
+#include "../app/SnakeApp.hpp"
+#include "../app/MinesweeperApp.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+
+namespace monolith::window {
+
+namespace {
+
+#include "detail/wm_body_01.inc"
+#include "detail/wm_body_02.inc"
+#include "detail/wm_body_03.inc"
+#include "detail/wm_body_04.inc"
+#include "detail/wm_body_05.inc"
+#include "detail/wm_body_06.inc"
+#include "detail/wm_body_07.inc"
+#include "detail/wm_body_08.inc"
+#include "detail/wm_body_09.inc"
